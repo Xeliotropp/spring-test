1078a1031e726be88da37d6313d81a3fcd5b1593
Xeliotropp <xeliotrop@gmail.com>
2023-09-23 19:31:33 +0300

    initial

---
+++ src/main/java/com/springsefte/springsefte/SpringSefteApplication.java
@@ -0,0 +1,13 @@
+package com.springsefte.springsefte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSefteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSefteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springsefte/springsefte/SpringSefteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsefte.springsefte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSefteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
